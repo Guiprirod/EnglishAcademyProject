3c69d07b3d6697ae3e3e95bee930541110ca8070
Guillermo Prieto Rodríguez <guilleprieto08@gmail.com>
2024-02-17 23:07:55 +0100

    New changes

--- EnglishAcademyProject/Program.cs
+++ EnglishAcademyProject/Program.cs
@@ -2,6 +2,7 @@ using EnglishAcademyProject.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
